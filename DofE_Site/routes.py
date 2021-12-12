66b7f3ba5781693c9bf094250fa78d19f67f2be8
openJCD <stuart.barton2017@gmail.com>
2021-12-12 14:24:44 +0000

    attempted grid layout for portfolio. will fix soon

--- DofE_Site/routes.py
+++ DofE_Site/routes.py
@@ -4,14 +4,28 @@ from DofE_Site import app
 posts = [
     {
         'title':'First Image',
+
         'image_url':'/static/sunset_placeholder.JPG',
+
         'description':'The first image to be uploaded.',
+
+        'grid-placement':'grid-columns:1/1;grid-rows:1/1;',
+
+        'portfolio-object':'square',
+
         'modal_id':'modal-1'
     },
     {
         'title':'Second Image',
+
         'image_url':'/static/seagull_wales.JPG',
+
         'description':'The second image to be uploaded.',
+
+        'grid-placement':'grid-columns:2/2;grid-rows:1/1;',
+
+        'portfolio-object':'rect',
+
         'modal_id':'modal-2'
     },
         {
@@ -30,10 +44,10 @@ def home():
 def featured():
     return render_template("featured.html", posts=posts)
 
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html", posts=posts)
+
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.route('/portfolio')
-def portfolio():
-    return render_template("portfolio.html")
